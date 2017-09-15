9774059bff66c35751a9d4678720a74c73237327
hh <hh@test.com>
2017-09-15 10:05:05 +0800

    appBar样式调整

--- src/components/appBar.vue
+++ src/components/appBar.vue
@@ -20,22 +20,39 @@
   }
 </script>
 
-<style scoped>
-  .app-bar-default {
+<style scoped lang="less">
+  .app-bar-common {
     height: 50px;
     line-height: 48px;
     width: 100%;
-    border: 1px solid lightgray;
-    background-color: rgb(51,51,51);
     text-align: center;
   }
+  .app-bar-default {
+    .app-bar-common;
+    border: 1px solid lightgray;
+    background-color: rgb(51,51,51);
+  }
   .app-bar-white {
-    height: 50px;
-    line-height: 48px;
-    width: 100%;
+    .app-bar-common;
     border: 1px solid lightgray;
     background-color: white;
-    text-align: center;
+  }
+
+  .app-bar-janshu-common {
+    border: 1px solid lightgray;
+    border-left: none;
+    border-right: none;
+    background-color: white;
+  }
+  .app-bar-janshu-top {
+    .app-bar-common;
+    .app-bar-janshu-common;
+    border-top: none;
+  }
+  .app-bar-janshu-bottom {
+    .app-bar-common;
+    .app-bar-janshu-common;
+    border-bottom: none;
   }
   .bar-left {
     float: left;
