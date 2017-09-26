e56f58f16edf565868b6f46607ab53e98bf3f04a
hh <hh@test.com>
2017-09-26 09:56:07 +0800

    添加overlay组件

---
+++ src/components/overlay.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="{ show: show}">
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapState({
+        show: state => state.ui.isShowOverlay
+      })
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .show {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
+    background-color: #000;
+    opacity: 0.4;
+  }
+</style>
--- src/vuex/modules/ui.js
+++ src/vuex/modules/ui.js
@@ -1,16 +1,23 @@
 export default {
   namespaced: true,
   state: {
-    isShowTransit: false
+    isShowTransit: false,
+    isShowOverlay: false
   },
   mutations: {
     SHOW_TRANSIT (state, show) {
       state.isShowTransit = show
+    },
+    SHOW_OVERLAY (state, show) {
+      state.isShowOverlay = show
     }
   },
   actions: {
     showTransit ({ commit }, show) {
       commit('SHOW_TRANSIT', show)
+    },
+    showOverlay ({ commit }, show) {
+      commit('SHOW_OVERLAY', show)
     }
   }
 }
