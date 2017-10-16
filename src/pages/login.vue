e08562fa10cc7e136845f0a0255bf81a206d649d
hh <hh@test.com>
2017-10-16 15:06:17 +0800

    移除无效元素

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -7,9 +7,6 @@
       <input v-model="accesstoken" class="token_input" placeholder="请输入accesstoken" type="text" id="accesstoken">
       <button @click="login" class="token_button">登录</button>
     </div>
-    <div class="panel">
-      <button class="scan_btn">扫描二维码登录</button>
-    </div>
   </div>
 </template>
 
@@ -55,13 +52,6 @@
   }
   .token_button {
     margin-left: 5px;
-  }
-  .scan_btn {
-    padding: 3px 3px;
-    cursor: pointer;
-    background-color: #80bd01;
-    border: 1px solid #eee;
-    border-radius: 3px;
   }
   .panel {
     padding: 10px 20px;
