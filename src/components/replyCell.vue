da113b92ac3526d7499d5670cae0b730ea053ac9
hh <hh@test.com>
2017-10-20 15:42:44 +0800

    调整replyCell的UI

--- src/components/replyCell.vue
+++ src/components/replyCell.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="cell">
-    <div class="author_content">
-      <router-link class="user_avatar" @click.native="getUserInfo" :to="{ name: '用户信息' }">
-        <img :src="reply.author.avatar_url" alt="avatar">
+    <div class="author-content">
+      <router-link @click.native="getUserInfo" :to="{ name: '用户信息' }">
+        <img class="avatar" :src="reply.author.avatar_url" alt="avatar">
       </router-link>
-      <div class="user_info">
+      <span class="user-info">
         <router-link @click.native="getUserInfo" :to="{ name: '用户信息' }">{{reply.author.loginname}}</router-link>
         <a :href="config.topicUrl + '#' + reply.id">
           {{ index + 1 }}楼•{{ reply.create_at }}
         </a>
-      </div>
-      <div class="user_action">
-        <i @click="ups" class="thumbs-up" title="喜欢">赞</i>
+      </span>
+      <span class="user-action">
+        <span @click="ups" class="thumbs-up" title="喜欢">赞</span>
         <span class="up-count">{{ reply.ups.length }}</span>
         <span @click="replyOther" class="reply">回复</span>
-      </div>
+      </span>
     </div>
     <div v-html="reply.content" class="reply_content">
     </div>
@@ -48,42 +48,24 @@
   }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
   .cell {
     background-color: #fff;
     font-size: 14px;
     text-align: left;
   }
-  .author_content {
-
-  }
-  .user_avatar {
-    display: inline-block;
-  }
-  .user_avatar img {
-    display: block; /** 解决img撑大a标签的问题 */
-    width: 30px;
-    height: 30px;
-    border-radius: 3px;
-  }
-  .user_info {
+  .user-info {
     margin: -15px 0 0 5px;
-    display: inline-block;
-    vertical-align: middle; /** 仅对inline、inline-block元素有效 */
+    vertical-align: baseline; /** 仅对inline、inline-block元素有效 */
   }
-  .user_info a {
+  .user-info a {
     text-decoration: none;
   }
-  .user_info a:first-child {
+  .user-info a:first-child {
     color: #444;
   }
-  .user_action {
+  .user-action {
     float: right;
-  }
-  .user_action i {
-    
-  }
-  .user_action i:before {
-    content: "\f087"
+    vertical-align: baseline;
   }
 </style>
