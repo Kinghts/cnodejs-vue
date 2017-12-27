51ddd3fb57c540ec1fd8e209438c49b42a0c50a9
ss <ss@mail.com>
2017-12-27 15:56:33 +0800

    简化路由的定义

--- src/config.js
+++ src/config.js
@@ -12,87 +12,39 @@ import Collect from './pages/collect'
 import Messages from './pages/messages'
 
 export default {
-  pages: {
-    home: {
-      name: '首页',
-      path: '/home',
-      redirect: true,
-      url: '/home/all'
-    },
-    detail: {
-      name: '详情',
-      path: '/detail',
-      redirect: false,
-      url: '',
-      component: Detail
-    },
-    create: {
-      name: '发布主题',
-      path: '/create',
-      redirect: false,
-      url: '',
-      component: EditTopic
-    },
-    editTopic: {
-      name: '编辑主题',
-      path: '/editTopic',
-      redirect: false,
-      url: '',
-      component: EditTopic
-    },
-    collections: {
-      name: '收藏',
-      path: '/collections',
-      redirect: false,
-      url: '',
-      component: Collect
-    },
-    message: {
-      name: '消息',
-      path: '/messages',
-      redirect: false,
-      component: Messages
-    }
-  },
   topics: {
     all: {
       name: '全部',
       hideTab: false,
-      path: '/home/all',
-      url: apiTopicsUrl + ''
+      path: '/home/all'
     },
     good: {
       name: '精华',
       hideTab: false,
-      path: '/home/good',
-      url: apiTopicsUrl + '?tab=good'
+      path: '/home/good'
     },
     share: {
       name: '分享',
       hideTab: false,
       path: '/home/share',
-      url: apiTopicsUrl + '?tab=share',
       create: true
     },
     ask: {
       name: '问答',
       hideTab: true,
       path: '/home/ask',
-      url: apiTopicsUrl + '?tab=ask',
       create: true
     },
     job: {
       name: '招聘',
       hideTab: true,
       path: '/home/job',
-      url: apiTopicsUrl + '?tab=job',
       create: true
     },
     dev: {
       name: '客户端测试',
       hideTab: true,
       path: '/home/dev',
-      url: apiTopicsUrl + '?tab=dev',
       create: true
     }
   },
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import Router from 'vue-router'
 import Main from '../pages/main'
 import Login from '../pages/login'
 import UserInfo from '../pages/userInfo'
-import config from '../config.js'
+import Detail from '../pages/detail'
+import EditTopic from '../pages/editTopic'
+import Collect from '../pages/collect'
+import Messages from '../pages/messages'
+import config from '../config'
 
 Vue.use(Router)
 
@@ -12,58 +16,22 @@ export default new Router({
 })
 
 function initRouters () {
-  let routers = []
-
-  // login
-  routers.push({
-    path: '/login',
-    name: '登录',
-    component: Login
-  })
-  // userinfo
-  routers.push({
-    path: '/userinfo',
-    name: '用户信息',
-    component: UserInfo,
-    beforeEnter: (to, from, next) => {
-      next(vm => { // 访问userinfo组件的实例
-        console.log(vm)
-      })
-    }
-  })
-
-  // pages
-  const _pages = config.pages
-  for (let page in _pages) {
-    if (_pages.hasOwnProperty(page)) {
-      let element = _pages[page]
-      let router = {
-        path: element.path,
-        name: element.name,
-        component: Main
-      }
-      if (element.redirect) {
-        router.redirect = element.url
-      }
-      if (element.component) {
-        router.component = element.component
-      }
-      routers.push(router)
-    }
-  }
+  let routers = [
+    { path: '/login', name: '登录', component: Login },
+    { path: '/userinfo', name: '用户信息', component: UserInfo },
+    { path: '/home', name: '首页', component: Main, redirect: '/home/all' },
+    { path: '/detail', name: '详情', component: Detail },
+    { path: '/create', name: '发布主题', component: EditTopic },
+    { path: '/editTopic', name: '编辑主题', component: EditTopic },
+    { path: '/collections', name: '收藏', component: Collect },
+    { path: '/messages', name: '消息', component: Messages }
+  ]
 
   // topics
-  const _topics = config.topics
-  for (let topic in _topics) {
-    if (_topics.hasOwnProperty(topic)) {
-      let element = _topics[topic]
-      routers.push({
-        path: element.path,
-        name: element.name,
-        component: Main
-      })
-    }
-  }
+  Object.entries(config.topics).map((v) => {
+    let topic = v[1]
+    routers.push({ path: topic.path, name: topic.name, component: Main })
+  })
 
   return routers
 }
